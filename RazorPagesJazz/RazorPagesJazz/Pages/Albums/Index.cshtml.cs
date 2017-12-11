e540dc9b6e62f3bf41fef00f0430c73b1f2a3ba8
Jude Battista <jbattista20@my.whitworth.edu>
2017-12-10 22:26:24 -0800

    Add additional query parameters for Albums

--- RazorPagesJazz/RazorPagesJazz/Pages/Albums/Index.cshtml.cs
+++ RazorPagesJazz/RazorPagesJazz/Pages/Albums/Index.cshtml.cs
@@ -24,7 +24,7 @@ namespace RazorPagesJazz.Pages.Albums
 		public SelectList Durations;
 		public string AlbumDuration { get; set; }
 
-        public async Task OnGetAsync(string albumTitle, string artistFName, string artistLname)
+        public async Task OnGetAsync(string albumTitle, string dateReleased, string artistFName, string artistLname, string trackName)
         {
 			//Create a queryable list of durations
 			/*
@@ -41,20 +41,76 @@ namespace RazorPagesJazz.Pages.Albums
 				albums = albums.Where(a => a.Title.Contains(albumTitle));
 			}
 
-			if (!String.IsNullOrEmpty(artistFName)) {
+			//Filter by datereleased
+			if (!String.IsNullOrEmpty(dateReleased))
+			{
+				albums = albums.Where(a => a.DateReleased.Equals(dateReleased));
+			}
+
+			//If the user queried on an artist's full name
+			if (!String.IsNullOrEmpty(artistFName) && !String.IsNullOrEmpty(artistLname))
+			{
 				var query =
-					(from a in _context.Albums
+					(from a in albums
 					 join f in _context.ArtistFeaturedOnAlbums on a.Id equals f.AlbumId
 					 join r in _context.Artists on f.ArtistId equals r.Id
-					 where r.Fname == artistFName
-					 select new Models.Albums{
+					 where r.Fname == artistFName && r.Lname == artistLname
+					 select new Models.Albums {
 						 Title = a.Title,
 						 Duration = a.Duration,
 						 DateReleased = a.DateReleased,
 					 });
 				albums = query;
-
 			}
+			//What if they only used the first name?
+			else if (!String.IsNullOrEmpty(artistFName)) 
+			{
+				var query =
+					(from a in albums
+					 join f in _context.ArtistFeaturedOnAlbums on a.Id equals f.AlbumId
+					 join r in _context.Artists on f.ArtistId equals r.Id
+					 where r.Fname == artistFName
+					 select new Models.Albums
+					 {
+						 Title = a.Title,
+						 Duration = a.Duration,
+						 DateReleased = a.DateReleased,
+					 });
+				albums = query;
+			}
+			else if (!String.IsNullOrEmpty(artistLname))
+			{
+				var query =
+					(from a in albums
+					 join f in _context.ArtistFeaturedOnAlbums on a.Id equals f.AlbumId
+					 join r in _context.Artists on f.ArtistId equals r.Id
+					 where r.Lname == artistLname
+					 select new Models.Albums
+					 {
+						 Title = a.Title,
+						 Duration = a.Duration,
+						 DateReleased = a.DateReleased,
+					 });
+				albums = query;
+			}
+
+			if (!String.IsNullOrEmpty(trackName))
+			{
+				var query = 
+					(from a in albums
+					 join c in _context.AlbumContainsTracks on a.Id equals c.AlbumId
+					 join t in _context.Tracks on c.TrackId equals t.Id
+					 where t.Name == trackName
+					 select new Models.Albums
+					 {
+						 Title = a.Title,
+						 Duration = a.Duration,
+						 DateReleased = a.DateReleased
+					 });
+				albums = query;
+			}
+
+
 			//query based on durations
 			/*
 			if (!String.IsNullOrEmpty(albumDuration)) {
