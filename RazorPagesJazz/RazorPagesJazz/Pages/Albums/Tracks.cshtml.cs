51cfa1885c528567322e82ef3bede3539c136013
Jude Battista <jbattista20@my.whitworth.edu>
2017-12-12 22:39:17 -0800

    Add the option to view tracks on an Album from the Album search results

---
+++ RazorPagesJazz/RazorPagesJazz/Pages/Albums/Tracks.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesJazz.Models;
+
+namespace RazorPagesJazz.Pages.Albums
+{
+	public class TracksModel : PageModel
+	{
+		private readonly RazorPagesJazz.Models.jazzRecordingDatabaseContext _context;
+
+		public TracksModel(RazorPagesJazz.Models.jazzRecordingDatabaseContext context)
+		{
+			_context = context;
+		}
+
+		public Models.Albums Albums { get; set; }
+		public IList<Models.Tracks> Tracks { get; set; }
+
+		//Note: Id here is an ALBUM ID, not a track ID. We want to get all the tracks on that album
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			var tracks = (
+				from t in _context.Tracks
+				join at in _context.AlbumContainsTracks on t.Id equals at.TrackId
+				join a in _context.Albums on at.AlbumId equals a.Id
+				where a.Id == id
+				select new Models.Tracks
+				{
+					Id = t.Id,
+					Name = t.Name,
+					Duration = t.Duration,
+					DateRecorded = t.DateRecorded
+				});
+
+			Tracks = await tracks.ToListAsync();
+
+			if (Tracks == null)
+			{
+				return NotFound();
+			}
+
+			Albums = await _context.Albums.SingleOrDefaultAsync(m => m.Id == id);
+
+			if (Albums == null)
+			{
+				return NotFound();
+			}
+			return Page();
+		}
+	}
+}
